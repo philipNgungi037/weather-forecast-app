07dda765816a3fd4e35bc56732960f7a6feaeecd
PHILIP <philophilo037@gmail.com>
2022-10-06 18:30:24 +0300

    search component done

--- my-app/src/components/search/search.js
+++ my-app/src/components/search/search.js
@@ -2,28 +2,41 @@ import { useState } from "react";
 import { AsyncPaginate } from "react-select-async-paginate";
 import { GEO_API_URL, geoApiOptions } from "../../api";
 
-const Search = ({onSearchChange}) => {
+const Search = ({ onSearchChange }) => {
     const [search, setSearch] = useState(null)
-    const handleOnChange = (searchData) =>{
+    const loadOptions = (inputvalue) => {
+        return fetch(
+            `${GEO_API_URL}/cities?minPopulation=1000000&namePrefix=${inputvalue}`,
+            geoApiOptions
+        )
+            .then(response => response.json())
+            .then((response) => {
+                return {
+                    options: response.data.map((city) => {
+                        return {
+                            value: `${city.latitude} ${city.longitude}`,
+                            label: `${city.name} ${city.countryCode}`,
+                        }
+                    })
+                }
+            })
+            .catch(err => console.error(err));
+    }
+    const handleOnChange = (searchData) => {
         setSearch(searchData);
         onSearchChange(searchData);
 
 
-        const loadOptions = (inputvalue) =>{
-            return fetch(`${GEO_API_URL}/cities?minPopulation=1000000namePrefix=${inputvalue}`, geoApiOptions)
-            .then(response => response.json())
-            .then(response => console.log(response))
-            .catch(err => console.error(err));
-        }
+
     }
 
-    return(
+    return (
         <AsyncPaginate
-        placeholder="Search for City"
-        debounceTimeout={600}
-        value = {search}
-        onChange = {handleOnChange}
-        loadOptions = {loadOptions}
+            placeholder="Search for City"
+            debounceTimeout={600}
+            value={search}
+            onChange={handleOnChange}
+            loadOptions={loadOptions}
 
 
 
